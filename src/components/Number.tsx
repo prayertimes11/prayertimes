b7e7332f30ba3d5a8351f9908d086ff6be348a75
prayertimes <prayertimes@proton.me>
2023-10-29 06:39:04 +0300

    use old es

--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -1,5 +1,13 @@
 export const Number = ({ value = 0 }) => {
-  const result = String(value).padStart(2, "0");
+  function zeroPad(value: number, length: number): string {
+    let valueStr = value.toString();
+    while (valueStr.length < length) {
+      valueStr = "0" + valueStr;
+    }
+    return valueStr;
+  }
+
+  const result = zeroPad(value, 2);
   return (
     <div className="digital">
       <p>88</p>
